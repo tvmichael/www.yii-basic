cbde8b8c2a5a6eca14a72bfdcd182d8754598454
tvmichael <tvmichael@meta.ua>
2018-06-28 22:59:00 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,17 @@
+;(function (window) {
+
+    if (window.JCCatalogElement)
+        return;
+
+    window.playPayExchange = function () {
+
+    };
+
+    window.playPayExchange.prototype =  {
+        init: function(params){
+            console.log(this);
+        },
+
+    };
+
+})(window);
