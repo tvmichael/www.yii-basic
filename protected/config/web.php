ff92249be01235fc3f0bd0c56582906b5d87cd2b
tvmichael <tvmichael@meta.ua>
2019-11-30 17:22:23 +0200

    url manager

--- protected/config/web.php
+++ protected/config/web.php
@@ -2,6 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
+$url = require __DIR__ . '/url.php';
 
 $config = [
     'id' => 'basic',
@@ -42,15 +43,10 @@ $config = [
                 ],
             ],
         ],
+
         'db' => $db,
 
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-
+        'urlManager' => $url,
     ],
     'params' => $params,
 ];
