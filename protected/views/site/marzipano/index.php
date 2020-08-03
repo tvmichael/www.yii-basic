71a505fefb337caba8dea6fda6c63f3d9ff12b7b
tvmichael <tvmichael@meta.ua>
2020-08-03 22:10:10 +0300

    Marzipano

---
+++ protected/views/site/marzipano/index.php
@@ -0,0 +1,17 @@
+<?php
+use yii\web\View;
+use app\assets\MarzipanoAsset;
+
+$this->title = 'Marzipano';
+MarzipanoAsset::register($this);
+
+?>
+<div class="site-select-2">
+    <div>
+        <h2>Marzipano</h2>
+    </div>
+    <div>
+        <?$k = (new \yii\db\Query())?>
+    </div>
+</div>
+
